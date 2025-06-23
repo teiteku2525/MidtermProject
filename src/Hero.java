f7b4dc7a73017b2a9ff1d6d5ae843a4863756293
Fujiwara takeru <241065@std.hi-joho.ac.jp>
2025-06-23 11:04:00 +0900

    Character内のattackを削除

--- src/Hero.java
+++ src/Hero.java
@@ -6,10 +6,10 @@ public class Hero extends Character{
         weapon = weaponName;
     }
 
-    /*public void attack(Character target) {
+    public void attack(Character target) {
         System.out.println(this.name + "は" + weapon + "で攻撃！" + target.name + "に10のダメージを与えた！");
         target.hp -= 10;
-    }*/
+    }
 
     public void heal() {
         System.out.println(this.name + "は回復呪文を唱えた！HPが20回復した！");
